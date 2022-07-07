5c26b3e9dd096d75b89eb862fb74dc36d5b1a7df
YDude <mrcrueloneman@gmail.com>
2022-07-07 15:49:45 +0300

    commit 6 TransitionGroup

--- src/Component/PostList.jsx
+++ src/Component/PostList.jsx
@@ -1,22 +1,31 @@
 import React from 'react';
 import PostItem from "./PostItem";
+import {CSSTransition, TransitionGroup} from "react-transition-group";
 
 const PostList = ({posts, title, remove}) => {
 
     if (!posts.length) {
         return (
-        <h1 style={{textAlign: 'center'}}>
-            Посты не найдены
-        </h1>
+            <h1 style={{textAlign: 'center'}}>
+                Посты не найдены
+            </h1>
         )
     }
 
     return (
         <div>
             <h1 className="header1" style={{textAlign: "center"}}>{title}</h1>
-            {posts.map((item, i) => (
-                <PostItem remove={remove} number={i + 1} key={item.id} post={item}/>
-            ))}
+            <TransitionGroup>
+                {posts.map((item, i) => (
+                    <CSSTransition
+                        key={item.id}
+                        timeout={500}
+                        classNames="post"
+                    >
+                    <PostItem remove={remove} number={i + 1}  post={item}/>
+                    </CSSTransition>
+                ))}
+            </TransitionGroup>
         </div>
     );
 };
